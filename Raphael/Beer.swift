072eac79351d59583e6708b4d037f55b034c9fd4
Raphael <raphael@MacBook-Air-de-Raphael.local>
2017-07-17 12:57:47 -0300

    Initial Commit

---
+++ Raphael/Beer.swift
@@ -0,0 +1,19 @@
+//
+//  Beer.swift
+//  Raphael
+//
+//  Created by Raphael Higashi Silva on 17/07/17.
+//  Copyright © 2017 raphaelhigashi. All rights reserved.
+//
+
+import UIKit
+
+class Beer {
+    var id: Int!
+    var name: String!
+    var abv: String!
+    var tagline: String!
+    var firstBrewed: String!
+    var description: String!
+    var imageUrl: String!
+}
